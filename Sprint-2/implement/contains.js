ce7075dc014840032678e91d95f3af91220982b2
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 20:27:09 +0100

    Implement 'contains' function with object validation
    Add validation to ensure 'contains' function receives a valid object.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(obj, propertyName) {
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    throw new Error("contains requires an object");
+  }
+  return Object.hasOwn(obj, propertyName);
+}
 
 module.exports = contains;
