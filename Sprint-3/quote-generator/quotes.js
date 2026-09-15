f0e1a0daa92ffa7294b51b851fb30dba6555dc37
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 20:56:25 +0100

    Implement random quote display functionality
    Added displayRandomQuote function to show a random quote and author on button click.

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -491,3 +491,19 @@ const quotes = [
 ];
 
 // call pickFromArray with the quotes array to check you get a random quote
+function displayRandomQuote() {
+  const selectedQuote = pickFromArray(quotes);
+
+  document.getElementById("quote").textContent = selectedQuote.quote;
+  document.getElementById("author").textContent = selectedQuote.author;
+}
+
+function setup() {
+  const newQuoteButton = document.getElementById("new-quote");
+
+  newQuoteButton.addEventListener("click", displayRandomQuote);
+
+  displayRandomQuote();
+}
+
+window.onload = setup;
