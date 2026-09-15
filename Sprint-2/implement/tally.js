fa5ef900b0ba3a66be37f3aa85b3b474f184744f
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 20:43:39 +0100

    Add tally function for counting item occurrences
    Implement tally function to count occurrences of items.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,15 @@
-function tally() {}
+function tally() {
+  const counts = {};
+
+  for (const item of items) {
+    if (!Object.hasOwn(counts, item)) {
+      counts[item] = 1;
+    } else {
+      counts[item]++;
+    }
+  }
+
+  return counts;
+}
 
 module.exports = tally;
