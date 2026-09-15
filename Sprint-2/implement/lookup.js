75e788d5589bee39b2f9fa3a479a05a7e0711fb3
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 20:31:47 +0100

    Modify createLookup to accept parameters
    Refactor createLookup to accept countryCurrencyPairs as an argument and return a lookup object.

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,8 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  // Object.fromEntries turns [[key, value], ...] into { key: value, ... }.
+  // It builds a brand-new object and leaves the input array untouched.
+  // If a country code appears more than once, the last pair wins.
+  return Object.fromEntries(countryCurrencyPairs);
 }
 
 module.exports = createLookup;
