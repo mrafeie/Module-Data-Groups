e5eeb1e882388541b4755acaf7dabc1f367aa39b
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 20:40:52 +0100

    Improve parseQueryString function
    Enhance query string parsing to handle empty pairs and decode components.

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,43 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
+
+  if (queryString === "") {
     return queryParams;
   }
-  const keyValuePairs = queryString.split("&");
 
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+  const pairs = queryString.split("&");
+
+  for (const pair of pairs) {
+    if (pair === "") {
+      continue;
+    }
+
+    const equalIndex = pair.indexOf("=");
+
+    let key;
+    let value;
+
+    if (equalIndex === -1) {
+      key = pair;
+      value = "";
+    } else {
+      key = pair.slice(0, equalIndex);
+      value = pair.slice(equalIndex + 1);
+    }
+
+    key = key.replace(/\+/g, " ");
+    value = value.replace(/\+/g, " ");
+
+    key = decodeURIComponent(key);
+    value = decodeURIComponent(value);
+
+    if (queryParams[key] === undefined) {
+      queryParams[key] = value;
+    } else if (Array.isArray(queryParams[key])) {
+      queryParams[key].push(value);
+    } else {
+      queryParams[key] = [queryParams[key], value];
+    }
   }
 
   return queryParams;
