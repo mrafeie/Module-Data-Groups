5b5d17c32bbe323f2e4010346d6e1dde45a2e4e5
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 18:05:55 +0100

    Enhance findMax function to handle non-numeric values

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,17 @@
 function findMax(elements) {
+  const numbers = elements.filter((item) => typeof item === "number");
+  if (numbers.length === 0) {
+    return -Infinity;
+  }
+  let max = -Infinity;
+
+  for (const num of numbers) {
+    if (num > max) {
+      max = num;
+    }
+  }
+
+  return max;
 }
 
 module.exports = findMax;
