4c91d5d7a0d4ef35018b445a6735ca526a117501
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 20:45:29 +0100

    Correct key-value inversion in invert function
    Fix the implementation of the invert function to correctly swap keys and values.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,20 +10,31 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
 
-// a) What is the current return value when invert is called with { a : 1 }
+console.log("invert.js loaded");
 
-// b) What is the current return value when invert is called with { a: 1, b: 2 }
+module.exports = invert;
 
-// c) What is the target return value when invert is called with {a : 1, b: 2}
+// a) What is the current return value when invert is called with {a : 1}?
+// The current return value when invert is called with {a: 1} is {key: 1}.
 
-// c) What does Object.entries return? Why is it needed in this program?
+// b) What is the current return value when invert is called with {a: 1, b: 2}?
+// The current return value when invert is called with {a: 1, b: 2} is {key: 2}.
 
-// d) Explain why the current return value is different from the target output
+// c) What is the target return value when invert is called with {a: 1, b: 2}?
+// The target return value when invert is called with {a: 1, b: 2} is {1: a, 2: b}.
 
-// e) Fix the implementation of invert (and write tests to prove it's fixed!)
+// d) What does Object.entries return? Why is it needed in this program?
+// Object.entries() returns an array containing the key-value pairs of an object.
+// It is needed in this program because the function must access both the keys and the values in order to swap them.
+
+// e) Explain why the current return value is different from the target output.
+// The current return value is different from the target output because invertedObj.key = value does not use the variable key as the property name.
+// Bracket notation is needed to swap the keys and values.
+
+// f) Fix the implementation of invert (and write tests to prove it's fixed!)
