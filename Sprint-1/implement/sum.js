2bade65323833607a255994f881c7b721367c7cd
mrafeie <m.rafeie.mr@gmail.com>
2026-09-15 18:06:52 +0100

    Enhance sum function to handle non-number elements

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,9 @@
 function sum(elements) {
+  const numbers = elements.filter((item) => typeof item === "number");
+  if (numbers.length === 0) {
+    return 0;
+  }
+  return numbers.reduce((total, num) => total + num, 0);
 }
 
 module.exports = sum;
